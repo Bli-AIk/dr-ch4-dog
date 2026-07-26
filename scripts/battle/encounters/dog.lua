89f98598916d26b90dc351ab978610376524e97b
Bli_Aik <haikun2333@126.com>
2026-07-26 18:54:17 +0800

    feat: 初始化dog遭遇战！

---
+++ scripts/battle/encounters/dog.lua
@@ -0,0 +1,26 @@
+
+local Dog, super = Class(Encounter)
+
+function Dog:init()
+    super.init(self)
+
+    -- Text displayed at the bottom of the screen at the start of the encounter
+    self.text = Game:loc("* what the dog...?", "encounter_dog_start")
+
+    -- Battle music ("battle" is rude buster)
+    self.music = "dog_buster"
+    -- Enables the purple grid battle background
+    self.background = true
+
+    -- Add the dummy enemy to the encounter
+    self:addEnemy("dummy")
+
+    --- Uncomment this line to add another!
+    --self:addEnemy("dummy")
+end
+
+function Dog:onBattleInit()
+    Game.battle.music:setVolume(0.5)
+end
+
+return Dog
