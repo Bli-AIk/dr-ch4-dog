f9137d549b3a86f8b0904099bc6cf944b120b604
Bli_Aik <haikun2333@126.com>
2026-07-27 18:17:19 +0800

    feat: 狗进行一个转和吸收

--- scripts/battle/waves/gb.lua
+++ scripts/battle/waves/gb.lua
@@ -1,18 +1,81 @@
 ---@class GB : Wave
 local GB, super = Class(Wave)
 
+-- 游戏使用的帧率
 local FPS = 30
+
+-- Dog spin 动画的帧数
 local SPIN_FRAME_COUNT = 27
+-- Dog spin 动画的循环次数
 local SPIN_LOOPS = 3
+-- Dog spin 动画的总时长
 local SPIN_TIME = (SPIN_FRAME_COUNT * SPIN_LOOPS) / FPS
+
+-- 圆形效果的持续时长
+local CIRCLE_TIME = SPIN_TIME * (SPIN_LOOPS - 1) / SPIN_LOOPS
+-- 圆形效果出现时的半径
+local CIRCLE_INITIAL_RADIUS = 300
+-- 圆形效果开始持续缩小时的半径
+local CIRCLE_START_RADIUS = 100
+-- 圆形效果的最小半径
+local CIRCLE_MIN_RADIUS = 0
+-- 圆形效果从大变小的时长
+local CIRCLE_INTRO_TIME = 9 / FPS
+-- 圆形绘制使用的分段数
+local CIRCLE_SEGMENTS = 96
+
+-- GB 滑入战斗框的时长
 local ENTRY_TIME = 1
-local WAIT_FRAMES = FPS
+-- GB 发射前的等待帧数
+local WAIT_FRAMES = FPS / 2
+-- GB 发射前的蓄力帧数
 local CHARGE_FRAMES = 3
+
+-- GB 光束的持续时长
 local BLAST_TIME = 45
+-- GB 光束的预热帧数
 local BLAST_WARMUP_FRAMES = 5
+-- GB 光束的消散帧数
 local BLAST_FADE_FRAMES = 20
+
+-- Dog 白色覆盖的褪去时长
 local WHITE_FADE_TIME = 0.5
+-- 回合结束前的额外缓冲时长
 local WAVE_BUFFER = 1 / 3
+
+-- 圆形使用的迷雾 shader 文件
+local MIST_SHADER_PATH = Mod.info.path .. "/assets/shaders/gb_mist.frag"
+
+local GBCircle, circle_super = Class(Object)
+
+function GBCircle:init(x, y, radius)
+    circle_super.init(self, x, y, radius * 2, radius * 2)
+
+    self.radius = radius
+    self.alpha = 0
+    self.white_amount = 0
+    self.time = 0
+    self.shader = love.graphics.newShader(MIST_SHADER_PATH)
+end
+
+function GBCircle:update()
+    circle_super.update(self)
+    self.time = self.time + DT
+end
+
+function GBCircle:draw()
+    local old_shader = love.graphics.getShader()
+    local old_r, old_g, old_b, old_a = love.graphics.getColor()
+
+    self.shader:send("time", self.time)
+    self.shader:send("white_amount", self.white_amount)
+
+    love.graphics.setShader(self.shader)
+    love.graphics.setColor(1, 1, 1, self.alpha)
+    love.graphics.circle("fill", 0, 0, self.radius, CIRCLE_SEGMENTS)
+    love.graphics.setColor(old_r, old_g, old_b, old_a)
+    love.graphics.setShader(old_shader)
+end
 
 local function getWaveTime()
     return SPIN_TIME
@@ -39,6 +102,37 @@ function GB:init()
     self.time = getWaveTime()
     self.dog = nil
     self.white_fx = nil
+    self.circle = nil
+end
+
+function GB:spawnCircle()
+    local center_x, center_y = self.dog:getRelativePos(self.dog.width / 2, self.dog.height / 2)
+    local circle = GBCircle(center_x, center_y, CIRCLE_INITIAL_RADIUS)
+    circle.layer = self.dog.layer - 1
+    self.circle = self:spawnObject(circle)
+
+    self.timer:tween(
+        CIRCLE_INTRO_TIME,
+        circle,
+        {radius = CIRCLE_START_RADIUS, alpha = 1},
+        "out-cubic",
+        function()
+            self.timer:tween(
+                CIRCLE_TIME - CIRCLE_INTRO_TIME,
+                circle,
+                {radius = CIRCLE_MIN_RADIUS, white_amount = 1},
+                "linear",
+                function()
+                    if circle.parent then
+                        circle:remove()
+                    end
+                    if self.circle == circle then
+                        self.circle = nil
+                    end
+                end
+            )
+        end
+    )
 end
 
 function GB:spawnBlaster()
@@ -111,6 +205,7 @@ function GB:onStart()
         return
     end
 
+    self:spawnCircle()
     self.white_fx = self.dog:addFX(ColorMaskFX({1, 1, 1}, 0), "gb_white")
     self.timer:tween(SPIN_TIME * (SPIN_LOOPS - 1) / SPIN_LOOPS, self.white_fx, {amount = 1}, "linear")
 
